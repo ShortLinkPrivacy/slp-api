4225acc44b5d36c2d475414c2db88965f00afb78
Stefan Geneshky <stefanguen@gmail.com>
2016-04-04 22:49:26 -0700

    Copy advanced message view

--- application.ts
+++ application.ts
@@ -38,24 +38,44 @@ function view(data) {
             #content { width: 500px; margin-left: auto; margin-right: auto; }
             .slp {}
             .slp a { color: #00f; font-weight: bold; }
-            pre { background-color: #eee; padding: 10px; border: 1px dotted #aaa; }
+            pre { background-color: #eee; padding: 10px; border: 1px dotted #aaa; overflow-y: auto; }
+            #decrypt { text-decoration: none; border-bottom: 1px dotted blue; font-size: .8em; }
         </style>
       </head>
       <body>
         <div id="content">
             <h1>PGP Encrypted Message</h1>
             <p class="slp">
-                Please install the <a href="">Short Link Privacy</a> browser
-                plugin to have this message seamlessly decrypt in your browser.
+                Please install the <a href="https://chrome.google.com/webstore/detail/short-link-privacy/kkhoekeemmabphdemkfakkjjmpfkjocm">Short Link Privacy</a> browser
+                plugin to have this message decrypt in your browser.
             </p>
-            <pre>
+            <div> <a id="decrypt" href="#">decrypt here</a> </div>
+            <pre id="msg">
             ` + data.body + `
             </pre>
             <small class="footer">
                 <a href="https://en.wikipedia.org/wiki/Pretty_Good_Privacy">What is PGP?</a> |
-                <a href="">What is SLP?</a>
+                <a href="/">What is SLP?</a>
             </small>
         </div>
+        <script>
+           document.getElementById('decrypt').addEventListener('click', function(e) {
+            e.preventDefault();
+            var el = document.createElement('pre');
+            el.innerText = location.href;
+            this.parentElement.appendChild(el);
+            this.parentElement.removeChild(this);
+           });
+
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-74656221-2', 'auto');
+          ga('send', 'pageview');
+
+        </script>
       </body>
     </html>
     `;
